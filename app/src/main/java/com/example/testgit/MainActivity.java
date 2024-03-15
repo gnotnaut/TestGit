db4aca03e3d7d2f76abbe67481ef8b16eec9d5b0
-global <ttong6913@gmail.com>
2024-03-15 21:07:43 +0700

    B2

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("B1");
+        System.out.println("B2");
 
     }
 }
