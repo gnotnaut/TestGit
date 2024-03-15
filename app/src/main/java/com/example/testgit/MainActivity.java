eea9ddc4c1cd7be588dde5821bfd0158020cd24a
-global <ttong6913@gmail.com>
2024-03-15 21:18:52 +0700

    A3

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("A1");
         System.out.println("A2");
+        System.out.println("A3");
 
     }
 }
