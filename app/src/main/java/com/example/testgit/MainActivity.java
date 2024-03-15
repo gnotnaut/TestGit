d916fa12bed310239ed1af8f131b7bced35ea11c
-global <ttong6913@gmail.com>
2024-03-15 21:35:57 +0700

    fig rebase

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -11,10 +11,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         System.out.println("A1");
         System.out.println("A2");
         System.out.println("A3");
 
+
+
+        System.out.println("B1");
 
     }
 }
